7a42fa3096cd000e86bcad1f508e57c104862346
HoneyPaptan <hpaptan@gmail.com>
2026-02-18 17:55:44 +0530

    01: added  roboto as variable font

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem={false}  
           storageKey="far-theme"
           enableColorScheme={true}
